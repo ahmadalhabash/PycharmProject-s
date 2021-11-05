b83850acb6dd73bb7047b74995d84a410a54ddc3
ahmadalhabsh <92648139+ahmadalhabash@users.noreply.github.com>
2021-11-05 19:59:29 +0200

    Update THe Heirs.py

--- THe Heirs.py
+++ THe Heirs.py
@@ -17,12 +17,13 @@ class heirs():
         if "0" in family or "1" in family or "2" in family or "3" in family or "4" in family or "5" in family or "6" in family or "7" in family \
                 or "8" in family or "9" in family or "!" in family or "@" in family or "#" in family or "%" in family or "^" in family \
                 or "&" in family or "*" in family or "(" in family or ")" in family or "_" in family or "+" in family or "-" in family \
-                or "=" in family or "~" in family or "`" in family  or money_account[10] >4 or money_account[10] <0:
+                or "=" in family or "~" in family or "`" in family or "" in family or money_account[10] >4 or money_account[10] <0:
             Lie_detector[0] = 0
             print("PLEASE WRITE THE CORRECT INFORMATION")
 
         else:
-            if "pass" in self.person[1] or "" == self.person[1] or " " == self.person[1] or "  " == self.person[1] or "   " == self.person[1] or "    " in self.person[1]:
+            if "pass" in self.person[1] or "" == self.person[1] or " " == self.person[1] \
+                    or "  " == self.person[1] or "   " == self.person[1] or "    " in self.person[1]:
                 the_share_of_the_heirs_per_person[0] = 0
 
             else:
@@ -31,7 +32,8 @@ class heirs():
                 print("....................")
                 the_share_of_the_heirs_per_person[0] = 20 / 100 * money_account[1]
 
-            if "pass" in self.person[3] or "" == self.person[3] or " " == self.person[3] or "  " == self.person[3] or "   " == self.person[3] or "    " in self.person[3]:
+            if "pass" in self.person[3] or "" == self.person[3] or " " == self.person[3] \
+                    or "  " == self.person[3] or "   " == self.person[3] or "    " in self.person[3]:
                 the_share_of_the_heirs_per_person[1] = 0
 
             else:
@@ -57,7 +59,8 @@ Lie_detector = [None]
 
 money_account = [print("Write the amount of the heirs :"), float(input()), print(".............."),
                  print("Write how many males :"), float(input()), print("..............."),
-                 print("Write how many females :"), float(input()) ,print("..............."), print("Write how many wifes :"), float(input())]
+                 print("Write how many females :"), float(input()) ,print("..............."), 
+                 print("Write how many wifes :"), float(input())]
 
 # jj Values
 dad = float()  # [0]
@@ -68,7 +71,8 @@ the_share_of_the_heirs_per_person = [float(dad), float(mam), float(wif)]
 
 print("...............")
 
-family = (print("Write the name of the father. If the father is not existent ,pass :"), input(),print("Write the name of the mother. If the mother is not existent ,pass:"), input())
+family = (print("Write the name of the father. If the father is not existent ,pass :"), input() ,
+          print("Write the name of the mother. If the mother is not existent ,pass :"), input())
 
 print("\n")
 
